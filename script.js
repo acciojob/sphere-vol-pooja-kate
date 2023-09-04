23092afec864f9f56869134e79a1e5e3781b4eaf
Pooja Kate <82284439+pooja-kate@users.noreply.github.com>
2023-09-04 11:45:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function volume_sphere() {
-    //Write your code here
+	const rad  =parseFloat(document.getElementById("radius").value);
+	if (isNaN(rad) || rad <= 0) {
+		
+		document.getElementById("volume").textContent = "Please enter a valid positive number for the radius.";
+} 
+	else {
+                // Calculate the volume of the sphere
+        const vol = (4/3) * Math.PI * Math.pow(rad, 3);
+document.getElementById("volume").textContent = ${vol.toFixed(2)};
+}
   
 } 
 
